13f791d818fa5b118e126c1811ee6407fbb5ea50
haliun0301 <haliungnbt31@gmail.com>
2023-07-27 14:58:36 +0800

    Khaliun

--- src/views/pages/crisis/medical/emergency.vue
+++ src/views/pages/crisis/medical/emergency.vue
@@ -2,91 +2,91 @@
   <main style="padding: 30px">
     <div id="MainTextCnt">
 
-      <div class="lifeH3"><h3 class="llText">Emergency (night-time, holidays)</h3></div>
+      <div class="lifeH3"><h3 class="llText">{{$t("emergency.head_title")}}</h3></div>
 
       <div class="w660 cenObj">
         <div class="padT20 marB20">
           <table cellspacing="5" cellpadding="0" border="0" class="lifeHeadMenu">
             <tbody><tr>
-              <td><a href="#1">How to call an ambulance</a></td>
-              <td><a href="#2">Nigh-time and Holiday's Medical Treatment</a></td>
+              <td><a href="#1">{{$t("emergency.link1")}}</a></td>
+              <td><a href="#2">{{$t("emergency.link2")}}</a></td>
             </tr>
             </tbody></table>
         </div>
 
-        <p>When abroad, getting sick or having an accident will make you feel very nervous. You'll be worried about; how to see a doctor, how to communicate in a foreign language in the hospital, how much the cost will be, who is going to take care of you etc. Moreover, because you'll catch a disease or get involved in an accident unexpectedly, there is no way to be informed beforehand. If the pain is bearable, you could afford the time to look for a hospital and go there the next day. But depending on your situation, you might need a urgent help to get to a hospital. To add to your anxiety to see a doctor in a foreign country, in an emergency you'll get into a panic.<br>
+        <p>{{$t("emergency.text1")}}<br>
           <br>
-          Here, we'll show what to do when you meet with an accident, how to call the ambulance and about the night-time and holidays' medical treatment.</p>
+          {{$t("emergency.text2")}}</p>
 
-        <div class="lifeH4 padT15"><h4><a id="1">How to call an ambulance</a></h4></div>
-        <p class="padT15">If you were in a life-threatening condition, call an ambulance immediately. See the following instruction.</p>
+        <div class="lifeH4 padT15"><h4><a id="1">{{$t("emergency.title1")}}e</a></h4></div>
+        <p class="padT15">{{$t("emergency.text1_1")}}</p>
 
         <ul class="padT15 padL15">
-          <li>1. Call "119".</li>
-          <li>2. You'll be asked "Fire or Emergency?" So, answer: "emergency".</li>
-          <li>3. Next, explain the situation correctly: what happened, when and where it happened, who is in trouble and the condition of that person right now.</li>
-          <li>4. Tell them where you are (address or any building easy to find nearby), your name and your phone number.</li>
-          <li>5. Ask what to do until an ambulance arrives.</li>
-          <li>6. Now, follow the instruction and wait.</li>
-          <li>7. When the ambulance arrives, tell the staff about the situation. It will be nice if someone accompanies the emergency patient. In that case, to take money and insurance card may help.</li>
+          <li>1. {{$t("emergency.li1_1")}}</li>
+          <li>2. {{$t("emergency.li1_2")}}</li>
+          <li>3. {{$t("emergency.li1_3")}}</li>
+          <li>4. {{$t("emergency.li1_4")}}</li>
+          <li>5. {{$t("emergency.li1_5")}}</li>
+          <li>6. {{$t("emergency.li1_6")}}</li>
+          <li>7. {{$t("emergency.li1_7")}}</li>
         </ul>
-        <p class="padT15">By the way, recently some people call the ambulance in a situation not that serious. For that reason, it is reported that in some cases the ambulance couldn't take action immediately and their arrival was delayed. The Tokyo Fire Department says: "Ambulance is the service to take the sick and injured to the hospital in an emergency. If one calls the ambulance not in an emergency, it is possible for us not being able to save the other who really needed help. If you aren't in a life-threatening condition, please use other transport to go to the hospital."</p>
+        <p class="padT15">{{$t("emergency.text1_2")}}</p>
 
         <div class="padT15">
           <ul class="helpSite helpbgimgEn">
-            <li><a href="http://www.tfd.metro.tokyo.jp/lfe/kyuu-adv/tksei01.html" target="_blank">Tokyo Fire Department "Information on the proper use of the ambulance"</a></li>
+            <li><a href="http://www.tfd.metro.tokyo.jp/lfe/kyuu-adv/tksei01.html" target="_blank">{{$t("emergency.text1_link1")}}</a></li>
           </ul>
         </div>
 
-        <p class="padT10">Then, in what kind of condition do we call the ambulance? To help this, the Tokyo Fire Department offers an "Emergency Consultation Center" to whom we may ask.<br>
+        <p class="padT10">{{$t("emergency.text1_3")}}<br>
           <br>
-          *"Emergency Consultation Center" phone: #7119 (24 hours, 365 days).<br>
+          {{$t("emergency.text1_4")}}<br>
           <br>
-          They will give you instruction such as: in what situation we need or don't need to call the ambulance, what kind of first-aid is needed, where the open hospital is and so on. Please call if you are in trouble.<br>
+          {{$t("emergency.text1_5")}}<br>
           <br>
-          Moreover, each fire station in Tokyo helps you find an emergency hospital by phone.</p>
+          {{$t("emergency.text1_6")}}</p>
 
         <div class="padT15">
           <ul class="helpSite helpbgimgEn">
-            <li><a href="http://www.tfd.metro.tokyo.jp/kb/sho.htm" target="_blank">Tokyo Fire Department: "Emergency Hospital Guidance by phone (List of the Fire Stations)".</a></li>
+            <li><a href="http://www.tfd.metro.tokyo.jp/kb/sho.htm" target="_blank">{{$t("emergency.text1_link2")}}</a></li>
           </ul>
         </div>
 
-        <p class="padT15">However, in the following situations, please call an ambulance immediately. You'll not be charged for this.</p>
+        <p class="padT15">{{$t("emergency.text1_7")}}</p>
         <ul class="padT15 padL15">
-          <li>1. not breathing, no pulse or unconscious</li>
-          <li>2. bleeding heavily</li>
-          <li>3. being cramped for more than 10 minutes</li>
-          <li>4. other obviously serious illness, not being able to move etc.</li>
+          <li>1. {{$t("emergency.li2_1")}}</li>
+          <li>2. {{$t("emergency.li2_2")}}</li>
+          <li>3. {{$t("emergency.li2_3")}}</li>
+          <li>4. {{$t("emergency.li2_4")}}</li>
         </ul>
 
-        <div class="lifeH4 padT15"><h4><a id="2">Nigh-time and Holiday's Medical Treatment</a></h4></div>
-        <p class="padT15">Medical institutions are usually closed on Sundays, holidays and at the end and the beginning of the year. Also, there are some hospitals taking summer vacation during the Bon Festival (in the mid August) or not offering service on Saturdays in the afternoon. In addition, the usual office hours are from 9 a.m. to 4 p.m.<br>
+        <div class="lifeH4 padT15"><h4><a id="2">{{$t("emergency.title2")}}</a></h4></div>
+        <p class="padT15">{{$t("emergency.text2_1")}}<br>
           <br>
-          Therefore, large hospitals offer emergency examination for the outpatients during night-time and holidays. However, not all of the institutions offer this service. The Web Site of the Tokyo Fire Department carries the list of emergency hospitals.</p>
+          {{$t("emergency.text2_2")}}</p>
 
         <div class="padT15">
           <ul class="helpSite helpbgimgEn">
-            <li><a href="http://www.tfd.metro.tokyo.jp/kb/index.htm" target="_blank">Emergency Hospital Information:</a></li>
+            <li><a href="http://www.tfd.metro.tokyo.jp/kb/index.htm" target="_blank">{{$t("emergency.text2_link1")}}</a></li>
           </ul>
         </div>
 
-        <p class="padT15">Moreover, in Tokyo "Metropolitan Information Service for Medical Institutions: Himawari" offers audio service by phone.<br>
-          Phone number: 03-5272-0303 (24 hours open).</p>
+        <p class="padT15">{{$t("emergency.text2_3")}}<br>
+          {{$t("emergency.text2_4")}}</p>
 
         <div class="padT15">
           <ul class="helpSite helpbgimgEn">
-            <li><a href="http://www.himawari.metro.tokyo.jp/qq/qq13tomnlt.asp" target="_blank">Tokyo Metropolitan Medical Institution Information</a></li>
+            <li><a href="http://www.himawari.metro.tokyo.jp/qq/qq13tomnlt.asp" target="_blank">{{$t("emergency.text2_link2")}}</a></li>
           </ul>
         </div>
 
-        <p class="padT15">When in emergency, you'll not be able to look for a medical institution in that situation. Therefore, we recommend you to check the institutions with emergency examination service in your neighborhood, and take notes. Moreover, please make sure you keep your insurance card with you.<br>
+        <p class="padT15">{{$t("emergency.text2_5")}}<br>
           <br>
-          The important point is that emergency examination for the outpatients does not mean "24/7 hospital". It is possible that special doctor is absent or there aren't sufficient numbers of doctors during those hours, which means in some cases you'll not get enough treatment. If you need to have a medical treatment in an emergency, please call the institution first and tell your situation, and then check if you really have to get treated right away.<br>
+          {{$t("emergency.text2_6")}}<br>
           <br>
-          In the emergency examination, you'll only get the first-aid and simple check from the doctor on duty. And you'll be told to come the next day to get the special doctor's examination.<br>
+          {{$t("emergency.text2_7")}}<br>
           <br>
-          By the way, when you call a hospital to get emergency examination, sometimes you'll be rejected for no doctor being available at that moment. Therefore we recommend you to check the contact phone number of more than one hospital.</p>
+          {{$t("emergency.text2_8")}}</p>
       </div>
     </div>
   </main>
--- src/views/pages/crisis/medical/sick.vue
+++ src/views/pages/crisis/medical/sick.vue
@@ -2,72 +2,73 @@
   <main style="padding: 30px">
     <div id="MainTextCnt">
 
-      <div class="lifeH3"><h3 class="llText">"When you get sick"</h3></div>
+      <div class="lifeH3"><h3 class="llText">{{$t("whenSick.title1")}}</h3></div>
 
       <div class="w660 cenObj">
         <div class="padT20 marB20">
           <table cellspacing="5" cellpadding="0" border="0" class="lifeHeadMenu">
             <tbody><tr>
-              <td><a href="#1">Which department to visit?</a></td>
-              <td><a href="#2">What to do if hospitalization is needed?</a></td>
+              <td><a href="#1">{{$t("whenSick.link1")}}</a></td>
+              <td><a href="#2">{{$t("whenSick.link2")}}</a></td>
             </tr>
             </tbody></table>
         </div>
 
-        <p>Having four different seasons here in Japan, we experience climate change each three months. If you aren't accustomed to the cycle of the seasons, it's highly possible you to get sick easily. Not taking it seriously will not only make your condition worse but also infects other people. Therefore, if you feel sick, please go to see a doctor as soon as possible.</p>
+        <p>{{$t("whenSick.text1")}}</p>
 
-        <div class="lifeH4 padT15"><h4><a id="1">Which department to visit?</a></h4></div>
-        <p class="padT15">Internal medicine: general cold, internal organ or influenza etc.<br>
-          Surgery: operation needed injuries or diseases<br>
-          Orthopedics: bones, joints, muscles, backache etc.<br>
-          Dermatology: diseases of skins such as eczema, nettle rash, burn, frostbite etc.<br>
-          Pediatrics: deals with children (from babies and infants to teenager) and their illness<br>
-          Obstetrics and Gynecology: illness peculiar to women, pregnancy and birth<br>
-          Otolaryngologist (ENT): diseases and illness of the ears, nose and throat<br>
-          Ophthalmology: diseases of the eyes<br>
-          Psychiatry: mental illness such as depression, anorexia, panic disorder etc.<br>
-          Plastic surgery: burn, frostbite, spot, mark, cosmetic surgery etc.<br>
+        <div class="lifeH4 padT15"><h4><a id="1">{{$t("whenSick.title2")}}</a></h4></div>
+        <p class="padT15">
+          
+          {{$t("whenSick.line2_1")}}<br>
+          {{$t("whenSick.line2_2")}}<br>
+          {{$t("whenSick.line2_3")}}<br>
+          {{$t("whenSick.line2_4")}}<br>
+          {{$t("whenSick.line2_5")}}<br>
+          {{$t("whenSick.line2_6")}}<br>
+          {{$t("whenSick.line2_7")}}<br>
+          {{$t("whenSick.line2_8")}}<br>
+          {{$t("whenSick.line2_9")}}<br>
+          {{$t("whenSick.line2_10")}}<br>
+
           <br>
-          Please make sure that you know the types of medical institutions in your neighborhood and what kind of treatment they offer. In case you don't know which institution to visit, please check your symptom referring the list above and consult the medical institution nearby, equipped with the department you think you need to visit, first by phone.</p>
+          {{$t("whenSick.text2_1")}}</p>
         <div class="padT15">
           <ul class="helpSite helpbgimgEn">
-            <li><a href="http://www.himawari.metro.tokyo.jp/qq/qq13enmnlt.asp" target="_blank">Tokyo Metropolitan Medical Institution Information "Himawari"."</a></li>
+            <li><a href="http://www.himawari.metro.tokyo.jp/qq/qq13enmnlt.asp" target="_blank">{{$t("whenSick.link2_1")}}</a></li>
           </ul>
         </div>
-        <p class="padT15">Although large general hospitals and hospitals in affiliation with universities have every kind of departments, the problem is that it's highly possible that you have to wait for quite a long time there.<br>
+        <p class="padT15">{{$t("whenSick.text2_2")}}<br>
           <br>
-          Moreover, without an introduction card written by a doctor of your neighborhood, the doctor's fee will be higher. Therefore, to see a doctor in your neighborhood will be best for the first step. If that hospital or clinic wasn't enough, the next step will be asking the doctor to write an introduction card and then go to a large general hospital.<br>
-          <br>
-          * Medical Institutions equipped with more than 20 beds are called "hospitals (byouin)", those with less than 19 beds are called "clinic (sinnryoujo, clinic, or iin in Japanese)".</p>
+          {{$t("whenSick.text2_3")}}</p>
 
-        <div class="lifeH4 padT15"><h4><a id="2">What to do if hospitalization is needed?</a></h4></div>
-        <p class="padT15">If you are told that you need to be hospitalized, please follow the instruction of the hospital. In general, they'll ask you things listed below:</p>
+        <div class="lifeH4 padT15"><h4><a id="2">{{$t("whenSick.title3")}}</a></h4></div>
+        <p class="padT15">{{$t("whenSick.text3_1")}}</p>
         <ul class="padT15 padL15">
-          <li>- application form, agreement</li>
-          <li>- hospital identification card</li>
-          <li>- health insurance card</li>
-          <li>- seal</li>
+          <li>- {{$t("whenSick.line3_11")}}</li>
+          <li>- {{$t("whenSick.line3_12")}}</li>
+          <li>- {{$t("whenSick.line3_13")}}</li>
+          <li>- {{$t("whenSick.line3_14")}}</li>
         </ul>
-        <p class="padT15">Moreover, when going into the hospital, guarantee and a deposit are required. Generally you'll ask your relatives, your school teacher or your boss to be your guarantee. If you cannot find anyone to ask, please consult the hospital. In some cases, paying additional deposit will make you entering the hospital without a guarantee. A deposit is usually between 50,000 yen to 100,000 yen. When getting out of the hospital, it'll be calculated as hospital charges and be adjusted, which means you need to keep all kind of deposit receipt till you get out.<br>
+        <p class="padT15">{{$t("whenSick.text3_2")}}<br>
           <br>
-          In addition, you need to prepare things listed below when going into the hospital:</p>
+          {{$t("whenSick.text3_3")}}</p>
 
         <ul class="padT15 padL15">
-          <li>- underwear and changes of clothes</li>
-          <li>- pajamas</li>
-          <li>- towels</li>
-          <li>- toilet articles such as toothbrush, soap, comb, shaver etc.</li>
-          <li>- slippers or sandals</li>
-          <li>- tissue paper</li>
-          <li>- chopsticks, spoon, glass</li>
-          <li>- sanitary protection (for women)</li>
+          <li>- {{$t("whenSick.line3_31")}}</li>
+          <li>- {{$t("whenSick.line3_32")}}</li>
+          <li>- {{$t("whenSick.line3_33")}}</li>
+          <li>- {{$t("whenSick.line3_34")}}</li>
+          <li>- {{$t("whenSick.line3_35")}}</li>
+          <li>- {{$t("whenSick.line3_36")}}</li>
+          <li>- {{$t("whenSick.line3_37")}}</li>
+          <li>- {{$t("whenSick.line3_38")}}</li>
         </ul>
-        <p class="padT15">More importantly, please refrain from using the mobile phone inside the hospital, for it'll cause problems on the medical instruments. Please use the pay phone, or if using the mobile phone is unavoidable, please go to the area such as lobby where it is permitted to use it.</p>
+        <p class="padT15">{{$t("whenSick.text3_4")}}</p>
 
-        <div class="lifeH5 padT15"><h5>Amenity Bed (Pay Bed)</h5></div>
-        <p class="padT15">Usually each rooms in the hospital is quite large such as for six people. If going into a smaller room or a private room, you'll be asked for additional charges. This is called the amenity bed cost. This can't be covered by insurance, not even part of it, and will be the patient's pay. In addition, the cost of amenity bed differs from hospital to hospital.<br>
+        <div class="lifeH5 padT15"><h5>{{$t("whenSick.title3_title")}}</h5></div>
+        <p class="padT15">{{$t("whenSick.text3_5")}}<br>
           <br>
-          Depending on the vacancies of the hospital, it's possible that there is only vacancy in the room which requires an amenity bed cost. Even though the rule says that hospitals can't collect the amenity bed cost without the agreement of the patient, please be careful when signing an agreement or application form. Please beware of the cost and sign it only after being satisfied about the content.</p>
+          {{$t("whenSick.text3_6")}}</p>
 
       </div>
     </div>
